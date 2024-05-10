db050b12887a6ca29b6417d682a684c9a1059248
Haomiao <haomiaojasonji@gmail.com>
2024-05-09 20:48:58 -0400

    finish first version

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Navbar as BootstrapNavbar, Nav, Form, FormControl, Button, InputGroup } from 'react-bootstrap';
 import { Search } from 'react-bootstrap-icons';
+import ImageLogo from './assets/image.png';
 
 interface NavbarProps {
   onSearch: (searchTerm: string) => void;
@@ -30,21 +31,23 @@ const Navbar: React.FC<NavbarProps> = ({
 
   return (
     <BootstrapNavbar expand="lg" className="mb-4">
-      <BootstrapNavbar.Brand>Image Gallery</BootstrapNavbar.Brand>
+      <BootstrapNavbar.Brand>
+        <img src={ImageLogo} alt="Eulerity" style={{ height: '40px' }} />
+      </BootstrapNavbar.Brand>
       <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
-      <BootstrapNavbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto" style={{ margin: '0 10px' }}>
-          <Button variant="outline-info" onClick={onSelectAll} className="mr-3" style={{ borderRadius: '20px', margin: '0 10px' }}>
+      <BootstrapNavbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+        <Nav className="ml-auto">
+          <Button variant="primary" onClick={onSelectAll} className="me-3">
             Select All
           </Button>
-          <Button variant="outline-info" onClick={onClearSelection} className="mr-3" style={{ borderRadius: '20px', margin: '0 10px' }}>
+          <Button variant="primary" onClick={onClearSelection} className="me-3">
             Clear Selection
           </Button>
-          <Button variant="outline-info" onClick={onSortByName} style={{ borderRadius: '20px', margin: '0 10px' }}>
+          <Button variant="primary" onClick={onSortByName} className="me-3">
             Sort by Name {sortOrder === 'asc' ? 'A-Z' : 'Z-A'}
           </Button>
         </Nav>
-        <Form onSubmit={handleSearchSubmit} className="d-flex ml-auto" style={{ padding: '10px'}}>
+        <Form onSubmit={handleSearchSubmit} className="d-flex" style={{ padding: '10px' }}>
           <InputGroup>
             <FormControl
               type="text"
@@ -54,12 +57,11 @@ const Navbar: React.FC<NavbarProps> = ({
               className="mr-2"
               style={{ minWidth: '300px', borderRadius: '20px 0 0 20px', marginRight: '0' }}
             />
-            <Button type="submit" variant="outline-info" style={{ borderRadius: '0 20px 20px 0' }}>
+            <Button type="submit" variant="primary" style={{ borderRadius: '0 20px 20px 0' }}>
               <Search />
             </Button>
           </InputGroup>
         </Form>
-
       </BootstrapNavbar.Collapse>
     </BootstrapNavbar>
   );
